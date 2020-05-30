37e7fa4d7c3a1ec7bce5cb33c7f931291331c474
TRANGTHUANKIET <thuankiet12@gmail.com>
2020-05-30 16:22:30 +0700

    create functional component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+
   return (
     <div>
       <h1>Hello, world!</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      <Welcome name='Kiệt'/>
     </div>
   );
 }
