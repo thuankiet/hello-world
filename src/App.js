e60e454c64d496b3c88688c8a9b48e265ab8e559
TRANGTHUANKIET <thuankiet12@gmail.com>
2020-05-30 16:55:48 +0700

    add class component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './App.css';
 
+class WelcomeWithClass extends React.Component {
+  render() {
+      return (
+          <div>
+              <h1>Hello, {this.props.name}</h1>
+          </div>
+      );
+  }
+}
+
 function App() {
 
   function Welcome(props) {
@@ -11,7 +21,8 @@ function App() {
     <div>
       <h1>Hello, world!</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      <Welcome name='Kiệt'/>
+      <Welcome name='Functional Component'/>
+      <WelcomeWithClass name='Class Components'/>
     </div>
   );
 }
